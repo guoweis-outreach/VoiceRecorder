3d71abee17af369026d6ab4c29367b1dad03f174
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2017-02-18 21:06:00 +0100

    Add very basic audio recording

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
+import $ from 'jquery';
+import MediaStreamRecorder from 'msr';
+
+let mediaRecorder = undefined;
+
+function onMediaSuccess(stream) {
+  console.log('success');
+  mediaRecorder = new MediaStreamRecorder(stream);
+  mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
+  mediaRecorder.ondataavailable = function () {
+    console.log('data available');
+  };
+  mediaRecorder.start(3000);
+}
+
+function onMediaError(e) {
+  console.log('media error', e);
+}
+
+
+$(document).ready(() => {
   console.log('Init');
+
+  $('#recording-start').click(() => {
+    navigator.getUserMedia({ audio: true }, onMediaSuccess, onMediaError);
+  });
+  $('#recording-stop').click(() => {
+    mediaRecorder.stop();
+  });
+  $('#recording-save').click(() => {
+    mediaRecorder.save();
+  });
 });
